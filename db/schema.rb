accb3c09406822dd2456b0318e6f1fb4e867bf1b
Hunter Braun <hunter.braun@gmail.com>
2015-03-09 20:12:39 -0700

    Add devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310011325) do
+ActiveRecord::Schema.define(version: 20150310024133) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.string   "item",       limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user_attr = {
+  email: 'test@test.com',
+  password: 'password',
+  password_confirmation: 'password'
+}
+
+user = User.new(user_attr)
+user.save!
+
+# Create tasks
+task_one = user.tasks.build(item: 'This thing')
+task_one.save!
+
+task_two = user.tasks.build(item: 'That thing')
+task_two.save!
+
+task_three = user.tasks.build(item: 'The other thing')
+task_three.save!
